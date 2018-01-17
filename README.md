3d6adac9c7d557f587d034388231b27c45cf9ac3
brayvid <blakerayvid@gmail.com>
2018-01-17 09:05:32 -0500

    upd

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 - Generates an array where each element is [unique_word, frequency, [timestamps]], sorted by decreasing frequency.
 - The MyActivity file is available at https://takeout.google.com/settings/takeout by selecting My Activity > Select specific activity data > Youtube, and following the prompts.
 - Only searched words are counted, not video titles. Common words are ignored.
-- JQuery, p5.js and p5.dom.js are used via CDN.
+- JQuery 1.12.4 is used via CDN.
 - The process could take several minutes depending on the size of the MyActivity file.
--- rankSearches.js
+++ rankSearches.js
@@ -5,91 +5,83 @@
   - Open the browser console and navigate to localhost. Wait up to a few minutes for results if the MyHistory file is large.
 */
 
-var rank = function(p){
+var rank = function(){
 
-  p.setup = function(){
-    p.noCanvas();
-    p.noLoop();
+  var list_from_file = document.querySelector("#data-element > div");
 
-    var list_from_file = document.querySelector("#data-element > div");
+  if(list_from_file == null){
+    console.log("No file found, exiting.");
+    return;
+  }
 
-    if(list_from_file == null){
-      console.log("No file found, exiting.");
-      return;
+  // Number of most recent events to scan for searches.
+  var len = list_from_file.childElementCount;
+
+  var searches_with_dates = [];
+
+  var selected_data;
+  var search_and_date;
+
+  for(var i = 1; i < len + 1; i++){
+    console.log("Running...("+i+"/"+len+")");
+    selected_data = list_from_file.querySelector("div:nth-child("+i+") > div > div:nth-child(2)");
+    if(selected_data.innerHTML.indexOf("Searched") != -1){
+      search_and_date = [selected_data.querySelector("a").innerHTML, Date.parse(selected_data.innerHTML.match("(?<=<br>).*")[0])];
+      console.log("Match!");
+      searches_with_dates.push(search_and_date);
     }
-
-    // Most recent entries to scan for searches.
-    var len = list_from_file.childElementCount;
-  
-    console.log("Scanning the latest "+len+" entries for searches.");
-
-    var searches_with_dates = [];
-    var selected_data;
-
-    for(var i = 1; i < len + 1; i++){
-      console.log("Running...("+i+"/"+len+")");
-      selected_data = list_from_file.querySelector("div:nth-child("+i+") > div > div:nth-child(2)");
-      if(selected_data.innerHTML.indexOf("Searched") != -1){
-        var search_and_date = [selected_data.querySelector("a").innerHTML, Date.parse(selected_data.innerHTML.match("(?<=<br>).*")[0])];
-        console.log("Match");
-        searches_with_dates.push(search_and_date);
-      }
-    };
-
-    console.log("Found "+searches_with_dates.length+" searches.");
- 
-    var flattened_words_with_dates = [];
-
-    for(var i = 0; i < searches_with_dates.length; i++){
-        var words_in_search = searches_with_dates[i][0].split(/(?:,| )+/);  // regex from https://stackoverflow.com/a/650031
-        var search_time_ms = searches_with_dates[i][1];
-
-        for(var j = 0; j < words_in_search.length; j++){
-          flattened_words_with_dates.push([words_in_search[j], search_time_ms]);
-        }
-    };
-
-    var unique_words = [];
-    var frequencies = {};
-    var dates = {};
-
-    var ignored_word_count = 0;
-
-    for(var i = 0; i < flattened_words_with_dates.length; i++){
-      var active_word = flattened_words_with_dates[i][0];
-      var active_date = flattened_words_with_dates[i][1];
-
-      if(!(words_to_ignore.includes(active_word))){   // words_to_ignore defined in external file
-        if(!(unique_words.includes(active_word))){
-          // Word has not appeared yet
-          unique_words.push(active_word);
-          frequencies[active_word] = 1;
-          dates[active_word] = [active_date];
-        }else{
-          // Word has appeared before
-          frequencies[active_word]++;
-          dates[active_word].push(active_date);
-        }
-      }else{
-        ignored_word_count++;
-      }
-    };
-    console.log("Unique words: "+unique_words.length+"\nTotal words: "+flattened_words_with_dates.length+"\nIgnored words: "+ignored_word_count);
-
-    var words_arr = [];
-
-    for(var key in frequencies){
-      words_arr.push([key, frequencies[key], dates[key]]);
-    };
-
-    // Sort words by frequency value
-    var sorted_words = words_arr.sort(function(a, b){return b[1]-a[1]; }); // https://gist.github.com/umidjons/9614157
-    
-    console.log("Printing results and saving JSON file.");
-    console.log(sorted_words);
-    p.save(sorted_words,"youtube_searches_ranked.json");
-
   };
-  
+
+  var flattened_words_with_dates = []; 
+
+  for(var i = 0; i < searches_with_dates.length; i++){
+      var words_in_search = searches_with_dates[i][0].split(/(?:,| )+/);  // regex from https://stackoverflow.com/a/650031
+      var search_time_ms = searches_with_dates[i][1];
+
+      for(var j = 0; j < words_in_search.length; j++){
+        flattened_words_with_dates.push([words_in_search[j], search_time_ms]);
+      }
+  };
+
+  var unique_words = [];
+  var frequencies = {};
+  var dates = {};
+
+  var ignored_word_count = 0;
+
+  for(var i = 0; i < flattened_words_with_dates.length; i++){
+    var active_word = flattened_words_with_dates[i][0];
+    var active_date = flattened_words_with_dates[i][1];
+
+    if(!(words_to_ignore.includes(active_word))){   // words_to_ignore defined in external file
+      if(!(unique_words.includes(active_word))){
+        // Word has not appeared yet
+        unique_words.push(active_word);
+        frequencies[active_word] = 1;
+        dates[active_word] = [active_date];
+      }else{
+        // Word has appeared before
+        frequencies[active_word]++;
+        dates[active_word].push(active_date);
+      }
+    }else{
+      ignored_word_count++;
+    }
+  };
+
+  console.log("Found "+searches_with_dates.length+" searches in "+len+" events");
+  console.log("Unique words: "+unique_words.length+"\nTotal words: "+flattened_words_with_dates.length+"\nIgnored words: "+ignored_word_count);
+
+  var words_arr = [];
+
+  for(var key in frequencies){
+    words_arr.push([key, frequencies[key], dates[key]]);
+  };
+
+  // Sort words by frequency value
+  var sorted_words = words_arr.sort(function(a, b){return b[1]-a[1]; }); // https://gist.github.com/umidjons/9614157
+
+  console.log(sorted_words);
+
 };
 
